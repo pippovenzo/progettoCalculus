fb2f2ccb2c02f44dcaf47966411a19b0266a9fd1
Filippo Venzo <venzofilippo@gmail.com>
2026-02-12 15:33:42 +0100

    cubic model and quadratic model

--- Codice/cubicmodel.m
+++ Codice/cubicmodel.m
@@ -13,7 +13,7 @@ t = anni - min(anni);  %ogni anno viene trasformato in un numero da 0 a n
 n = length(t); %quanti anni considerati
 
 % Modello 1: Lineare
-X1 = [ones(n, 1), t]; %matrice del modello lineare: colonna di 1 e colonna con gli anni
+X1 = [ones(n, 1), t, t.^2, t.^3]; %matrice del modello lineare: colonna di 1 e colonna con gli anni
 
 figure('Color', 'w', 'Position', [100, 100, 900, 600]);
 hold on;
@@ -32,7 +32,7 @@ R2 = 1 - sum(residuals.^2) / sum((hdm - mean(hdm)).^2); %Coefficiente di determi
 % Generazione curva per il plot
 t_grid = linspace(min(t), max(t), 200)'; %griglia di 200 punti equidistanti per plottare il grafico
 anni_grid = t_grid + min(anni); %riporta gli anni al formato orignali in millenni
-X1_plot = [ones(200, 1), t_grid]; %crea la griglia per il grafico
+X1_plot = [ones(200, 1), t_grid, t_grid.^2, t_grid.^3]; %crea la griglia per il grafico
 y_plot = X1_plot * beta; %calcolo dei punti da plottare sugli anni veri con beta calcolata precedentemente
 
 plot(anni_grid, y_plot, 'Color', 'r', 'LineWidth', 2, ...
