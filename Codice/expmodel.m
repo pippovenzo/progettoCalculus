92e4147a1a0d6496491d620081bfeb34e47cf5e4
Simone Zecchinato <simone.zecchi4to@gmail.com>
2026-02-15 11:00:06 +0100

    .

--- Codice/expmodel.m
+++ Codice/expmodel.m
@@ -20,7 +20,6 @@ plot(anni, hdm, 'ko', 'MarkerFaceColor', 'k', 'DisplayName', 'Dati Reali (CSV)')
 % Calcolo fitting dati esponenziale (y = A * e^(B*t))
 % Linearizziamo prendendo il logaritmo naturale di hdm
 X1 = [ones(n, 1), t]; %matrice del modello lineare: colonna di 1 e colonna con gli anni
-beta = X1 \ hdm;  %risoluzione ai minimi quadratici per trovare il valore dei coefficenti beta
 beta_exp = X1 \ log(hdm);  % I coefficienti minimizzano i residui del logaritmo
 
 % Calcolo della predizione riportata in scala originale (esponenziale)
@@ -33,14 +32,14 @@ R2_exp = 1 - sum(residuals_exp.^2) / sum((hdm - mean(hdm)).^2); % R^2
 t_grid = linspace(min(t), max(t), 200)'; %griglia di 200 punti equidistanti per plottare il grafico
 anni_grid = t_grid + min(anni); %riporta gli anni al formato orignali in millenni
 X1_plot = [ones(200, 1), t_grid]; %crea la griglia per il grafico
-y_plot_exp = exp(X1_plot * beta_exp); 
+y_plot_exp = exp(X1_plot * beta_exp);
 plot(anni_grid, y_plot_exp, 'Color', 'r', 'LineWidth', 2, ...
-     'DisplayName', sprintf('%s (R^2: %.3f)', 'Esponenziale', R2_exp));
+     'DisplayName', sprintf('%s (R^2: %.3f) (MSE: %.3f)', 'Esponenziale', R2_exp, MSE_exp));
 
 % Cosmetica del Grafico 2
-title('Fitting Esponenziale - Nathan''s Famous Hot Dog Eating Contest');
+title('Fitting Esponenziale - Nathan''s Hot Dog Eating Contest');
 xlabel('Anno');
-ylabel('Numero di Hot Dog Mangiati (HDM)');
+ylabel('HDM');
 legend('Location', 'northwest');
 grid on;
 hold off;
